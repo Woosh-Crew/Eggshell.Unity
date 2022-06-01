08553cc2093ed99d83f21dbff6b69aeb906b34f4
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-06-01 22:34:04 +1000

    Remove Tools, Update Operators to reflect changes from core

---
+++ Editor/Operators/SaveScenes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor.SceneManagement;
+
+namespace Eggshell.Unity.Operators
+{
+	/// <summary>
+	/// This will save all open and modified scenes without asking if it
+	/// should or not.
+	/// </summary>
+	[Library( "eggshell.ops.scenes.save" ), Title( "Save Modified Scenes" )]
+	public class SaveScenes : Operator
+	{
+		public override bool Valid()
+		{
+			return EditorSceneManager.sceneCount != 0;
+		}
+
+		protected override void OnExecute()
+		{
+			EditorSceneManager.SaveOpenScenes();
+		}
+	}
+}
