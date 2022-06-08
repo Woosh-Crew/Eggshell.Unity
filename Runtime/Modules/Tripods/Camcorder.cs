ff6ebd8de8363cf1ff7ed16d43aa79ee7751bb0f
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-06-08 20:03:46 +1000

    Add interpolation tripod setup option

--- Runtime/Modules/Tripods/Camcorder.cs
+++ Runtime/Modules/Tripods/Camcorder.cs
@@ -6,115 +6,116 @@ using UnityEngine.Rendering.PostProcessing;
 
 namespace Eggshell.Unity
 {
-	public sealed class Camcorder : Module, Game.Callbacks
-	{
-		public Camera Camera { get; private set; }
+    public sealed class Camcorder : Module, Game.Callbacks
+    {
+        public Camera Camera { get; private set; }
 
-		#if POST_FX
+#if POST_FX
 
-		public PostProcessLayer Layer { get; private set; }
-		public PostProcessDebug Debug { get; private set; }
+        public PostProcessLayer Layer { get; private set; }
+        public PostProcessDebug Debug { get; private set; }
 
-		#endif
+#endif
 
-		// Runtime Game
-		// --------------------------------------------------------------------------------------- //
+        // Runtime Game
+        // --------------------------------------------------------------------------------------- //
 
-		public void OnPlaying()
-		{
-			Builder = Engine.Game.Components.Get<Tripod.Builder>();
+        public void OnPlaying()
+        {
+            Builder = Engine.Game.Components.Get<Tripod.Builder>();
 
-			if ( Builder == null )
-			{
-				// Not valid Game
-				return;
-			}
+            if (Builder == null)
+            {
+                // Not valid Game
+                return;
+            }
 
-			// Setup Camera
-			var go = new GameObject( "Camera" );
-			go.AddComponent<AudioListener>();
+            // Setup Camera
+            var go = new GameObject("Camera");
+            go.AddComponent<AudioListener>();
 
-			Camera = go.AddComponent<Camera>();
-			Camera.depth = 5;
+            Camera = go.AddComponent<Camera>();
+            Camera.depth = 5;
 
-			#if POST_FX
+#if POST_FX
 
-			// Setup Post Processing (Builtin Postfx)
+            // Setup Post Processing (Builtin Postfx)
 
-			Layer = go.AddComponent<PostProcessLayer>();
-			Layer.Init( UnityEngine.Resources.Load<PostProcessResources>( "PostProcessResources" ) );
+            Layer = go.AddComponent<PostProcessLayer>();
+            Layer.Init(UnityEngine.Resources.Load<PostProcessResources>("PostProcessResources"));
 
-			Layer.volumeTrigger = go.transform;
-			Layer.volumeLayer = LayerMask.GetMask( "TransparentFX", "Water" );
-			Layer.antialiasingMode = PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing;
+            Layer.volumeTrigger = go.transform;
+            Layer.volumeLayer = LayerMask.GetMask("TransparentFX", "Water");
+            Layer.antialiasingMode = PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing;
 
-			Debug = go.AddComponent<PostProcessDebug>();
-			Debug.postProcessLayer = Layer;
+            Debug = go.AddComponent<PostProcessDebug>();
+            Debug.postProcessLayer = Layer;
 
-			#endif
+#endif
 
-			GameObject.DontDestroyOnLoad( go );
-		}
+            GameObject.DontDestroyOnLoad(go);
+        }
 
-		public void OnExiting()
-		{
-			if ( Builder == null )
-			{
-				// Not valid Game
-				return;
-			}
+        public void OnExiting()
+        {
+            if (Builder == null)
+            {
+                // Not valid Game
+                return;
+            }
 
-			GameObject.Destroy( Camera.gameObject );
-			Setup = Default;
+            GameObject.Destroy(Camera.gameObject);
+            Setup = Default;
 
-			Camera = null;
-			Builder = null;
-		}
+            Camera = null;
+            Builder = null;
+        }
 
-		// Camcorder Loop
-		// --------------------------------------------------------------------------------------- //
+        // Camcorder Loop
+        // --------------------------------------------------------------------------------------- //
 
-		public Tripod.Builder Builder { get; private set; }
+        public Tripod.Builder Builder { get; private set; }
 
-		public Tripod.Setup Setup { get; private set; } = Default;
+        public Tripod.Setup Setup { get; private set; } = Default;
 
-		private static Tripod.Setup Default => new()
-		{
-			FieldOfView = 68,
-			Rotation = Quaternion.identity,
-			Position = Vector3.zero,
-		};
+        private static Tripod.Setup Default => new()
+        {
+            FieldOfView = 68,
+            Rotation = Quaternion.identity,
+            Position = Vector3.zero,
+        };
 
-		public override void OnUpdate()
-		{
-			if ( Terminal.Editor || Camera == null || Builder == null )
-			{
-				// Modules run in Editor, so don't do anything.
-				return;
-			}
+        public override void OnUpdate()
+        {
+            if (Terminal.Editor || Camera == null || Builder == null)
+            {
+                // Modules run in Editor, so don't do anything.
+                return;
+            }
 
-			var setup = Setup;
+            var setup = Setup;
 
-			// Default FOV
-			setup.FieldOfView = 68;
-			setup.Clipping = new( 0.1f, 700 );
+            // Default FOV
+            setup.FieldOfView = 68;
+            setup.Clipping = new(0.1f, 700);
 
-			// Build the setup, from game.
-			Builder.Build( ref setup );
-			Apply( Camera, setup );
+            // Build the setup, from game.
+            Builder.Build(ref setup);
+            Apply(Camera, setup);
 
-			Setup = setup;
-		}
+            Setup = setup;
+        }
 
-		private void Apply( Camera camera, Tripod.Setup setup )
-		{
-			var trans = camera.transform;
-			trans.localPosition = setup.Position;
-			trans.localRotation = setup.Rotation;
+        private void Apply(Camera camera, Tripod.Setup setup)
+        {
+            var trans = camera.transform;
+            trans.position = setup.Interpolation > 0 ? Vector3.Lerp(trans.position, setup.Position, setup.Interpolation * Time.deltaTime) : setup.Position;
+            trans.rotation = setup.Interpolation > 0 ? Quaternion.Slerp(trans.rotation, setup.Rotation, setup.Interpolation * Time.deltaTime) : setup.Rotation;
 
-			camera.fieldOfView = setup.Damping > 0 ? Mathf.Lerp( camera.fieldOfView, setup.FieldOfView, setup.Damping * Time.deltaTime ) : setup.FieldOfView;
-			camera.farClipPlane = setup.Clipping.y;
-			camera.nearClipPlane = setup.Clipping.x;
-		}
-	}
+            camera.fieldOfView = setup.Damping > 0 ? Mathf.Lerp(camera.fieldOfView, setup.FieldOfView, setup.Damping * Time.deltaTime) : setup.FieldOfView;
+
+            camera.farClipPlane = setup.Clipping.y;
+            camera.nearClipPlane = setup.Clipping.x;
+        }
+    }
 }
--- Runtime/Modules/Tripods/Tripod.cs
+++ Runtime/Modules/Tripods/Tripod.cs
@@ -3,163 +3,166 @@ using UnityEngine;
 
 namespace Eggshell.Unity
 {
-	/// <summary> 
-	/// A Tripod is Eggshell.Unity's camera system. It acts kinda like a wrapper for a singleton camera
-	/// pattern, Which allows us to be super performant. You use the Tripod for manipulating the Main Camera
-	/// every frame (Called in the Late Update Loop). You can easily swap them out on the client
-	/// </summary>
-	[Group( "Tripods" )]
-	public class Tripod : IObject
-	{
-		public Library ClassInfo { get; }
+    /// <summary> 
+    /// A Tripod is Eggshell.Unity's camera system. It acts kinda like a wrapper for a singleton camera
+    /// pattern, Which allows us to be super performant. You use the Tripod for manipulating the Main Camera
+    /// every frame (Called in the Late Update Loop). You can easily swap them out on the client
+    /// </summary>
+    [Group("Tripods")]
+    public class Tripod : IObject
+    {
+        public Library ClassInfo { get; }
 
-		public Tripod()
-		{
-			ClassInfo = Library.Register( this );
-		}
+        public Tripod()
+        {
+            ClassInfo = Library.Register(this);
+        }
 
-		~Tripod()
-		{
-			Library.Unregister( this );
-		}
+        ~Tripod()
+        {
+            Library.Unregister(this);
+        }
 
-		// Tripod Callbacks
-		// --------------------------------------------------------------------------------------- //
+        // Tripod Callbacks
+        // --------------------------------------------------------------------------------------- //
 
-		/// <summary>
-		/// Called every Tripod update. Used to control the main camera,
-		/// without any overhead of complications.
-		/// </summary>
-		public virtual void OnBuild( ref Setup camSetup ) { }
+        /// <summary>
+        /// Called every Tripod update. Used to control the main camera,
+        /// without any overhead of complications.
+        /// </summary>
+        public virtual void OnBuild(ref Setup camSetup) { }
 
-		/// <summary>
-		/// Called when the Tripod is now the active one, use this
-		/// for snapping your tripod to its initial position and rotation.
-		/// </summary>
-		public virtual void Activated( ref Setup camSetup ) { }
+        /// <summary>
+        /// Called when the Tripod is now the active one, use this
+        /// for snapping your tripod to its initial position and rotation.
+        /// </summary>
+        public virtual void Activated(ref Setup camSetup) { }
 
-		/// <summary>
-		/// Called when the tripod goes out of use. Use this for cleaning
-		/// up resources if need be.
-		/// </summary>
-		public virtual void Deactivated() { }
+        /// <summary>
+        /// Called when the tripod goes out of use. Use this for cleaning
+        /// up resources if need be.
+        /// </summary>
+        public virtual void Deactivated() { }
 
-		// Setup Buffer
-		// --------------------------------------------------------------------------------------- //
+        // Setup Buffer
+        // --------------------------------------------------------------------------------------- //
 
-		/// <summary>
-		/// A Tripod.Setup is responsible for controlling how the
-		/// main camera gets manipulated in the world. Tripod setups
-		/// are built every frame (In the Late Update Loop).
-		/// </summary>
-		public struct Setup
-		{
-			/// <summary> Camera's FOV </summary>
-			public float FieldOfView;
+        /// <summary>
+        /// A Tripod.Setup is responsible for controlling how the
+        /// main camera gets manipulated in the world. Tripod setups
+        /// are built every frame (In the Late Update Loop).
+        /// </summary>
+        public struct Setup
+        {
+            /// <summary> Camera's FOV </summary>
+            public float FieldOfView;
 
-			/// <summary> FieldOfView Damping </summary>
-			public float Damping;
+            /// <summary> FieldOfView Damping </summary>
+            public float Damping;
 
-			/// <summary> The position of the camera </summary>
-			public Vector3 Position;
+            /// <summary> Position and Rotation Interpolation </summary>
+            public float Interpolation;
 
-			/// <summary> The rotation of the camera </summary>
-			public Quaternion Rotation;
+            /// <summary> The position of the camera </summary>
+            public Vector3 Position;
 
-			/// <summary> Clipping Planes, X = Near, Y = Far </summary>
-			public Vector2 Clipping;
-		}
+            /// <summary> The rotation of the camera </summary>
+            public Quaternion Rotation;
 
-		// Builder Component
-		// --------------------------------------------------------------------------------------- //
+            /// <summary> Clipping Planes, X = Near, Y = Far </summary>
+            public Vector2 Clipping;
+        }
 
-		/// <summary>
-		/// A Tripod.Builder is responsible for controlling the logic
-		/// of how tripods are controlled and built. Override this in your
-		/// game to implement your own custom logic.
-		/// </summary>
-		public class Builder : IComponent<Game>
-		{
-			protected Tripod Current { get; set; }
+        // Builder Component
+        // --------------------------------------------------------------------------------------- //
 
-			public virtual void Created( Camera camera )
-			{
-				camera.renderingPath = RenderingPath.DeferredShading;
-				camera.gameObject.AddComponent<FlareLayer>();
-			}
+        /// <summary>
+        /// A Tripod.Builder is responsible for controlling the logic
+        /// of how tripods are controlled and built. Override this in your
+        /// game to implement your own custom logic.
+        /// </summary>
+        public class Builder : IComponent<Game>
+        {
+            protected Tripod Current { get; set; }
 
-			public virtual void Build( ref Setup setup )
-			{
-				var cam = Active();
+            public virtual void Created(Camera camera)
+            {
+                camera.renderingPath = RenderingPath.DeferredShading;
+                camera.gameObject.AddComponent<FlareLayer>();
+            }
 
-				if ( Current != cam )
-				{
-					Current?.Deactivated();
-					Current = cam;
-					Current?.Activated( ref setup );
-				}
+            public virtual void Build(ref Setup setup)
+            {
+                var cam = Active();
 
-				Current?.OnBuild( ref setup );
-				OnSetup( ref setup );
-			}
+                if (Current != cam)
+                {
+                    Current?.Deactivated();
+                    Current = cam;
+                    Current?.Activated(ref setup);
+                }
 
-			protected virtual Tripod Active() { return null; }
+                Current?.OnBuild(ref setup);
+                OnSetup(ref setup);
+            }
 
-			protected virtual void OnSetup( ref Setup setup )
-			{
-				Effect.Apply( ref setup );
-			}
+            protected virtual Tripod Active() { return null; }
 
-			// IComponent			
+            protected virtual void OnSetup(ref Setup setup)
+            {
+                Effect.Apply(ref setup);
+            }
 
-			public void OnAttached( Game item ) { }
-		}
+            // IComponent			
 
-		// Effect
-		// --------------------------------------------------------------------------------------- //
+            public void OnAttached(Game item) { }
+        }
 
-		/// <summary>
-		/// Tripod modifiers are temporary modifiers that change the Tripod setup 
-		/// after all the tripods and viewmodels have been built. This allows
-		/// you to do cool stuff like screen shake, or on land effects.
-		/// </summary>
-		public abstract class Effect
-		{
-			private static readonly List<Effect> All = new();
+        // Effect
+        // --------------------------------------------------------------------------------------- //
 
-			/// <summary>
-			/// Applies all the modifiers that are currently in the modifier
-			/// stack and removes them if we're done with it
-			/// </summary>
-			public static void Apply( ref Setup setup )
-			{
-				for ( var i = All.Count; i > 0; i-- )
-				{
-					var remove = All[i - 1].Update( ref setup );
+        /// <summary>
+        /// Tripod modifiers are temporary modifiers that change the Tripod setup 
+        /// after all the tripods and viewmodels have been built. This allows
+        /// you to do cool stuff like screen shake, or on land effects.
+        /// </summary>
+        public abstract class Effect
+        {
+            private static readonly List<Effect> All = new();
 
-					if ( remove )
-					{
-						All.RemoveAt( i - 1 );
-					}
-				}
-			}
+            /// <summary>
+            /// Applies all the modifiers that are currently in the modifier
+            /// stack and removes them if we're done with it
+            /// </summary>
+            public static void Apply(ref Setup setup)
+            {
+                for (var i = All.Count; i > 0; i--)
+                {
+                    var remove = All[i - 1].Update(ref setup);
 
-			/// <summary>
-			/// Clears all the modifiers from the stack, no ifs or buts. Just
-			/// pisses them all off.
-			/// </summary>
-			public static void Clear()
-			{
-				All.Clear();
-			}
+                    if (remove)
+                    {
+                        All.RemoveAt(i - 1);
+                    }
+                }
+            }
 
-			public Effect()
-			{
-				All.Add( this );
-			}
+            /// <summary>
+            /// Clears all the modifiers from the stack, no ifs or buts. Just
+            /// pisses them all off.
+            /// </summary>
+            public static void Clear()
+            {
+                All.Clear();
+            }
 
-			/// <returns> True if were done with this Modifier </returns>
-			protected abstract bool Update( ref Setup setup );
-		}
-	}
+            public Effect()
+            {
+                All.Add(this);
+            }
+
+            /// <returns> True if were done with this Modifier </returns>
+            protected abstract bool Update(ref Setup setup);
+        }
+    }
 }
